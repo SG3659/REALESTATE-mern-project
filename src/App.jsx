fd1c5d601a7b2bc3134c42c5e62d89d274abdd0c
sahil gupta <8278087870sahil@gmail>
2024-02-01 14:50:29 +0530

    <create pages and nav bar >

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
+import Profile from "../pages/Profile";
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-      <div>hii</div>
+      <div>
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/singup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </>
   );
 }
