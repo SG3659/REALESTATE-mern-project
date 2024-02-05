3f92e8e70b4bb42c78c06437ac2916c03953f01b
sahil gupta <8278087870sahil@gmail>
2024-02-05 19:44:14 +0530

    Docs:connect to data base

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongo from "mongoose";
+
+mongo.connect(
+  "mongodb+srv://sahil:gupta@real-estate.en9dyrm.mongodb.net/real-state?retryWrites=true&w=majority"
+).then(()=>{
+  console.log("connected to MongoDB");
+})
+.catch((error)=>{
+  console.log("error")
+})
 const app = express();
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
-
