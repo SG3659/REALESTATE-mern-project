71a78d8c60dc5b66d94ee340d03b78e58d68c64e
sahil gupta <8278087870sahil@gmail>
2024-02-07 16:22:02 +0530

    Docs: create a test api route

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}, './pages/**/*.{html,js}','./components/**/*.{html,js}',",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
